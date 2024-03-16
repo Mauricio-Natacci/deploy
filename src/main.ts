21163fbd19a09cb38e3acedf4adb7b543a4e1a06
Mauricio Natacci <mauricionatacci98@gmail.com>
2024-03-15 22:32:00 -0300

    remove nestjs config library

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,9 @@
-import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const configService = app.get(ConfigService);
-  const PORT = configService.get<number>('PORT');
+  const PORT = process.env.PORT || 3000;
   await app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 }
 bootstrap();
