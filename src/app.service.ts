7794edc98ca614f29683dcc2a38a0c2f9d98cda7
Mauricio Natacci <mauricionatacci98@gmail.com>
2024-03-16 10:17:45 -0300

    change to res.send

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Res } from '@nestjs/common';
+import { Response } from 'express';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello(@Res() res: Response) {
+    res.send('Hello World!');
   }
 }
